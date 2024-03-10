235824bb01ccc96f53a7d6d81e7880fb5057bb58
@ChezzySanchez <luderio.rubis.sanchez@gmail.com>
2024-03-10 08:31:53 +0800

    Code Push | COmpleted the Implement Bubble Sort challenge

---
+++ src/05-implement-bubble-sort/implement-bubble-sort-do-while-loop.ts
@@ -0,0 +1,40 @@
+/**
+ * A function that will handle the switching of elements to sort.
+ * @param {number[]} inputArray An array of numbers to be switched. 
+ * @returns {number[]} Returns nothing. Just switches the input array of numbers.
+ */
+const swap = (inputArray: number[]): number[] => {
+    return [inputArray[0], inputArray[1]] = [inputArray[1], inputArray[0]];
+}
+
+/**
+ * The main function. Accepts an array of unsorted array and returns the sorted array. 
+ * It copies the input array inside the function to prevent the input array to be modified (following pure function principles)
+ * executing a nested loop of do-while and for loop to loop through the unsorted (inputArray) array. 
+ * It utilizes the swap function to swap the array of elements if the left element is larger than the right.
+ * changing the element on the inputArray at every swap. 
+ * @param {number[]} array Accepts an array of unsorted numbers.
+ * @returns {number[]} Returns the sorted array of numbers.
+ */
+function bubbleSort(array: number[]): number[] {
+    const inputArray = [...array]; // copies the input array. 
+
+    let swapped: boolean = false;
+    do {
+        swapped = false;
+        for(let i = 0; i < inputArray.length; i++) {
+            if(inputArray[i] > inputArray[i+1]) {
+                let swappedElements = swap([inputArray[i], inputArray[i+1]]);
+                inputArray.splice(i, 1, swappedElements[0]);
+                inputArray.splice(i+1, 1, swappedElements[1]);
+                swapped = true;
+            }
+        }
+    }
+
+    while(swapped);
+
+    return inputArray;
+  }
+
+  bubbleSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]);
---
+++ src/05-implement-bubble-sort/takeaways.md
@@ -0,0 +1,21 @@
+# Key Takeaways: Implement Bubble Sort - Coding Interview Challenges
+### Completion time: 1 Hour
+### Completed on: March 10, 2024
+
+## Comments:
+
+I find this challenge to be easy than last time because I already know the sorting algorithm to be used. 
+
+The last time I did this, I'm still new to programming. I just got all my certification from Freecodecamp.org finished and I find Bubble Sort a bit intimidating back then. Last time, I also tried exploring alot ways to sort the input without following the ***"Bubble Sort"*** idea hence it takes me a long time to complete it. 
+
+As of now, I believe it still took me a bit of time to complete the challenge and that is because I solved that code two-ways using the ***while*** and ***do-while*** loop. 
+
+**Here are the steps I did to address the challenge:**
+
+1. I created a function called ***swap()*** to handle the swapping of elements on the unsorted array input.
+2. On the ***bubbleSort()*** function, I copied the input array and stored it in ***inputArray variable*** because I am trying to follow the ***Pure Function*** principle.  I then declared a nested loop ***(while/do-whle and a for loop)*** to handle the itiration on the input array. 
+3. Inside the nested loop function, i utilized the swap function declared earlier. I then spliced the inputAray elements replacing the original value with the swapped values. 
+4. I return the sorted inputArray. 
+
+***Found on the folder directory is the two approaches I wrote on solving the challenge.***
+
