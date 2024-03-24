723bb5401fd2ad1c897ea2d6dd4fd24b8588e4b7
@ChezzySanchez <luderio.rubis.sanchez@gmail.com>
2024-03-24 13:16:56 +0800

    Added a comments for dicumentation on the .ts file

--- src/07-implement-insertion-sort/implement-insertion-sort.ts
+++ src/07-implement-insertion-sort/implement-insertion-sort.ts
@@ -1,17 +1,22 @@
-
+/**
+ * A function that takes a array of numbers and returns the sorted version of the input.
+ * @param {number[]} array array of numbers
+ * @returns {number[]} array of numbers
+ */
 function insertionSort(array: number[]): number[] {
     let inputArray: number[] = [...array];
 
-    for(let i = 1; i < inputArray.length; i++) {
+    for(let i = 1; i < inputArray.length; i++) { // loops on the input array from the first element to last.
         let currentValue = inputArray[i];
         let j: number;
-        for(j = i - 1; j >= 0 && inputArray[j] > currentValue; j--) {
-            inputArray[j + 1] = inputArray[j];
+        for(j = i - 1; j >= 0 && inputArray[j] > currentValue; j--) { // loops on the input array from the current inputArray[i] index backwards
+            inputArray[j + 1] = inputArray[j]; // moves the index of the sorted array to the right by one index
         }
-        inputArray[j+1] = currentValue;
+
+        // inserts the current inputArray[i] value to the sorted index if the sorted index value is greater than the current value
+        inputArray[j+1] = currentValue; 
     }
 
-    console.log(inputArray)
     return array;
   }
 
